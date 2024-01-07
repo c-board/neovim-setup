5f831cf1d349dd71921f8c573a07d255277093b7
Colin Boardway <cmboardway@gmail.com>
2024-01-07 09:32:05 -0600

    cleanup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,15 @@
 -- Add any additional options here
 
 -- keep neotree closed by Default
-
 local api = vim.api
 api.nvim_set_var('neotree_auto_open', 0)
 
+-- enable line wrap
 local opt = vim.opt
 opt.wrap = true
 
+-- remove line background highlight
 vim.opt.cursorlineopt = 'number'
 
+-- remove netrw top banner
 vim.g.netrw_banner = 0
